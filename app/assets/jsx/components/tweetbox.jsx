e58cc0f63ea3aaef6ae462f0938deba5ba63eecf
Drew Cain <drew@simpli.fi>
2017-02-19 17:30:57 -0600

    added bootstrap styling to tweetbox

--- app/assets/jsx/components/tweetbox.jsx
+++ app/assets/jsx/components/tweetbox.jsx
@@ -1,12 +1,16 @@
 export default class TweetBox extends React.Component {
   render() {
     return (
-      <div>
-        <form>
-          <textarea />
-          <label>What's happening?</label>
-          <button>Tweet</button>
-        </form>
+      <div className="row">
+        <div className="col">
+          <form>
+            <div className="form-group">
+              <label for="tweetbox">What's happening?</label>
+              <textarea className="form-control" id="tweetbox" rows="3" />
+            </div>
+            <button type="submit" className="btn btn-primary">Tweet</button>
+          </form>
+        </div>
       </div>
     );
   }
