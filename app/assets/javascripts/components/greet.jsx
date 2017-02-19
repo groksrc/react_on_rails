5611218189c969e89ff90f77ef9912530b6ac200
Drew Cain <drew@simpli.fi>
2017-02-19 15:16:19 -0600

    added Greet component and package.json

---
+++ app/assets/javascripts/components/greet.jsx
@@ -0,0 +1,5 @@
+class Greet extends React.Component {
+  render() {
+    return <h2>Hello There</h2>;
+  }
+}
--- app/assets/javascripts/components/main.jsx
+++ app/assets/javascripts/components/main.jsx
@@ -1,7 +1,7 @@
 class Main extends React.Component {
   render() {
     return (
-      <h1>Hello from the Component</h1>
+      <Greet />
     );
   }
 }
